adba84f07cc24d0e645c521d195794c184261154
silviapetrino <petrino.silvia@gmail.com>
2023-09-21 13:57:34 +0200

    [add]: members array

---
+++ js/script.js
@@ -0,0 +1,46 @@
+// # js-our-team
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team. Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+
+
+
+
+// 1 - salvo i dati forniti in un array di oggetti;
+
+const membersTeam = [
+  {
+    name: "Wayne Barnett",
+    role: "Founder & CEO",
+    photo: "wayne-barnett-founder-ceo.jpg"
+  },
+  {
+    name: "Angela Caroll",
+    role: "Chief Editor",
+    photo: "angela-caroll-chief-editor.jpg"
+  },
+  {
+    name: "Walter Gordon",
+    role: "Office Manager",
+    photo: "walter-gordon-office-manager.jpg"
+  },
+  {
+    name: "Angela Lopez",
+    role: "Social Media Manager",
+    photo: "angela-lopez-social-media-manager.jpg"
+  },
+  {
+    name: "Scott Estrada",
+    role: "Developer",
+    photo: "scott-estrada-developer.jpg"
+  },
+  {
+    name: "Barbara Ramos",
+    role: "Graphic Designer",
+    photo: "barbara-ramos-graphic-designer.jpg"
+  }
+]
+
+console.log(membersTeam)
+
+
+// 2 - con un ciclo stampo le informazioni dentro il DOM con l'ultilizzo del Template literals (Template strings);
+
